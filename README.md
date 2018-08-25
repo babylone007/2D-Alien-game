4a107a30e898a9e549a2cc04026043cc141f27b7
babylone007 <babyloneroot>
2018-08-25 14:53:07 +0200

    Modifying Settings and Readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 2D-Alien-game
+# 2D-Alien-game with pygame library (Python 3)
+    It's a small 2D game in which player have a limited number of ships and when
+    a ship is hited by an alien the player will lose, the player will also lose
+    if the one or more aliens reach the screen bottom.
+
+# UML Digrams
--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ class Settings():
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
 
         # Alien Settings
-        self.fleet_drop_speed = 100
+        self.fleet_drop_speed = 10
 
         # How quickly the game speed up
         self.speedup_scale = 1.1
